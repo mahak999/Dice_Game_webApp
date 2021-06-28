35e9aa8b2991e2bbfa5aa49cb1855f4757313482
mahak99 <mahakkhurmi9@gmail.com>
2021-06-28 11:46:08 +0530

    changes made

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ right_img = "images/dice" + randomNumber2 + ".png";
 document.getElementsByClassName("img2")[0].src = right_img;
 
 if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 }
 else if (randomNumber1 < randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
 else {
     document.querySelector("h1").innerHTML = "Draw!";
