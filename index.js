a75b6be2eba9a842c35e25011bb2065590c03a63
mahak99 <mahakkhurmi9@gmail.com>
2021-06-28 11:12:43 +0530

    game logic added using javascript

---
+++ index.js
@@ -0,0 +1,19 @@
+randomNumber1 = Math.floor((Math.random() * 6) + 1);
+left_img = "images/dice" + randomNumber1 + ".png";
+document.getElementsByClassName("img1")[0].src = left_img;
+
+randomNumber2 = Math.floor((Math.random() * 6) + 1);
+right_img = "images/dice" + randomNumber2 + ".png";
+document.getElementsByClassName("img2")[0].src = right_img;
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+};
+
+if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+};
+
+if (randomNumber1 == randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw!";
+};
