0d9477ac71dd3768bb955f8324257c1fe1b6dc6e
mahak99 <mahakkhurmi9@gmail.com>
2021-06-28 11:14:37 +0530

    javascript file cleaned

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ document.getElementsByClassName("img2")[0].src = right_img;
 
 if (randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
-};
-
-if (randomNumber1 < randomNumber2){
+}
+else if (randomNumber1 < randomNumber2){
     document.querySelector("h1").innerHTML = "Player 2 Wins!";
-};
-
-if (randomNumber1 == randomNumber2){
+}
+else {
     document.querySelector("h1").innerHTML = "Draw!";
 };
